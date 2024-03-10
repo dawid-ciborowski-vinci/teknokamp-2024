1dc3f1f8b67da2f14c07223ea3c62faebe30ced3
cedric-bit <57269810+cedric-bit@users.noreply.github.com>
2024-03-10 10:04:31 +0100

    update
    yes

--- js-frontend-boilerplate-main/src/Components/Navbar/Navbar.js
+++ js-frontend-boilerplate-main/src/Components/Navbar/Navbar.js
@@ -8,22 +8,27 @@ const Navbar = () => {
 };
 
 function renderNavbar() {
+
   const navbar = document.querySelector('#navbarWrapper');
 
-  navbar.innerHTML = `
-    <nav class="navbar navbar-expand-lg navbar-light bg-light rounded">
-      <a class="navbar-brand mx-3" href="#">${SITE_NAME}</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse justify-content-between mx-3" id="navbarsExample09">
-        <ul class="navbar-nav">
-        </ul>
-        <button class="btn btn-success my-2 my-sm-0" type="button">Profil</button>
+  navbar.innerHTML =  `
+  <nav class="navbar navbar-expand-lg navbar-light bg-info">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">${SITE_NAME}</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
       </div>
     </nav>
-  `;
+`;
 }
 
 export default Navbar;
--- js-frontend-boilerplate-main/src/Components/Pages/UserPage.js
+++
@@ -1,7 +0,0 @@
-const UserPage = () => {
-    const main = document.querySelector('main');
-    main.innerHTML = `<h3>Welcome to your Eco-Responsability page!</h3>`;
-  };
-  
-  export default UserPage;
-  
